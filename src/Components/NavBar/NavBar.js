2a3df7217d2bee8c76a850804d57dd8aaaab4321
Abolfazl Omrani <abolfazl.omrani1999@gmail.com>
2020-02-05 01:08:59 +0330

    update navbar

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,13 +1,14 @@
 import React from 'react'
-import './NavBar.css'
 import {Link} from 'react-router-dom'
 
 const NavBar = props =>{
     return(
-        <ul id='navbar'>
-            <Link id='Link' to='/'><li>Home</li></Link>
-            <Link id='Link' to='/register'><li>Register</li></Link>
-        </ul>
+        <nav className="flex justify-between bb b--white-10">
+            <div className="center flex-grow pa3 flex items-center">
+            <Link id='Link' to='/'><a className="bold link dib white dim mr3 mr4-ns" href="#0">خانه</a></Link>
+            <Link id='Link' to='/register'><a className="bold link dib white dim mr3 mr4-ns" href="#0">ثبت نام</a></Link>
+            </div>
+        </nav>
     )
 }
 export default NavBar
